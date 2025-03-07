5adf0be2da9bc757266e37f51cfda023b72fb6c3
uosef <uosefahmed0022@gmail.com>
2025-03-07 23:11:49 +0200

    Replace ImageUrl with PdfUrl in Certificate model
    Updated the Certificate model and related classes to replace the ImageUrl property with PdfUrl. This change affects multiple files, including DownloadCertificateQueryHandler.cs, Certificate.cs, and CertificateDto.cs. A migration has been created to rename the database column from ImageUrl to PdfUrl to reflect this update.

--- ZadElealm.Core/Models/Certificate.cs
+++ ZadElealm.Core/Models/Certificate.cs
@@ -12,7 +12,7 @@ namespace ZadElealm.Core.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        public string PdfUrl { get; set; }
         public DateTime Compliateddate { get; set; }
         public string UserId { get; set; }
         public AppUser User { get; set; }
--- ZadElealm.Core/Models/ServiceDto/CertificateDto.cs
+++ ZadElealm.Core/Models/ServiceDto/CertificateDto.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        public string PdfUrl { get; set; }
         public DateTime CompletedDate { get; set; }
         public string UserName { get; set; }
         public string QuizName { get; set; }
