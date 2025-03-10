f9166a7fbec45958a2f0e71f8ca53420a133c033
uosef <uosefahmed0022@gmail.com>
2025-03-11 01:49:26 +0200

    Enhance Admin Dashboard functionality and UI
    - Added Login, AddAdmin, and Logout methods in AdminController.
    - Improved validation and error handling in Login and Edit views.
    - Updated navigation in _Layout.cshtml to include Categories link.
    - Enhanced Index views for users and categories with new features.
    - Created new CategoryController for category management.
    - Introduced AddAdmin form with validation and responsive design.
    - Added custom styles for improved UI in various CSS files.
    - Defined AdminDto for admin user creation data transfer.

---
+++ ZadElealm.Apis/Dtos/Auth/AdminDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ZadElealm.Apis.Dtos.Auth
+{
+    public class AdminDto
+    {
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
