06f52a2a72ef2eca18cff432d39fcebbb6d869ba
uosef <uosefahmed0022@gmail.com>
2025-03-10 18:52:50 +0200

    Add role management feature to Admin Dashboard
    Updated project file to include Microsoft.VisualStudio.Web.CodeGeneration.Design package.
    Introduced RoleController for managing roles with ASP.NET Core Identity, along with RoleFormViewModel and RoleViewModel for data handling.
    Created views for listing, creating, and updating roles, including validation.
    Added CSS for styling role management interfaces.

---
+++ AdminDashboard/Controllers/RoleController.cs
@@ -0,0 +1,85 @@
+﻿using AdminDashboard.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace AdminDashboard.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var roles =await _roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(RoleFormViewModel model) 
+        {
+            if(ModelState.IsValid)
+            {
+                var roleExists =await _roleManager.RoleExistsAsync(model.Name);
+                if (!roleExists)
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(model.Name));
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ModelState.AddModelError("Name", "Role already exists!");
+                    return View("Index", await _roleManager.Roles.ToListAsync());
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                await _roleManager.DeleteAsync(role);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Update(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            var mappedRole = new RoleViewModel()
+            {
+                Name = role.Name
+            };
+
+            return View(mappedRole);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(string id, RoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = await _roleManager.RoleExistsAsync(model.Name);
+                if (!role)
+                {
+                    var roleToUpdate = await _roleManager.FindByIdAsync(id);
+                    roleToUpdate.Name = model.Name;
+                    await _roleManager.UpdateAsync(roleToUpdate);
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ModelState.AddModelError("Name", "Role already exists!");
+                    return View("Index", await _roleManager.Roles.ToListAsync());
+                }
+            }
+                return RedirectToAction(nameof(Index));
+        }
+    }
+}
