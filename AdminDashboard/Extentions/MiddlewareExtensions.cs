65a0cc0c9d3fe7cd2f690bbf53367b36f0d302c4
uosef <uosefahmed0022@gmail.com>
2025-03-10 16:24:29 +0200

    Enhance jQuery and Bootstrap with new features and RTL support
    This commit includes significant updates to the jQuery library (v3.6.0) and Bootstrap framework (v5.1.0). Key changes include:
    
    - New methods for form data serialization in jQuery, improving AJAX functionality.
    - Enhanced support for JSONP requests and cross-domain operations.
    - Introduction of utility classes and RTL support in Bootstrap, allowing for better layout management and responsiveness.
    - Updates to source maps for easier debugging and maintenance.
    - Licensing information added to various files for legal clarity.
    
    These changes aim to improve the usability, performance, and maintainability of both libraries, providing developers with powerful tools for modern web development.

---
+++ AdminDashboard/Extentions/MiddlewareExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZadElealm.Repository.Data.Datbases;
+using ZadElealm.Repository.Data.SeedData;
+
+namespace AdminDashboard.Extentions
+{
+    public static class MiddlewareExtensions
+    {
+        public static async Task ConfigureMiddlewareAsync(this WebApplication app)
+        {
+            app.UseStatusCodePagesWithRedirects("/errors/{0}");
+            app.UseCors("Open");
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            #region Update the database automatically
+            using var scope = app.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var logger = services.GetRequiredService<ILogger<Program>>();
+
+            try
+            {
+                var appDbContext = services.GetRequiredService<AppDbContext>();
+                await appDbContext.Database.MigrateAsync().ConfigureAwait(false);
+                await AppDbContextSeed.SeedAsync(appDbContext, logger).ConfigureAwait(false);
+                logger.LogInformation("AppDbContext migrated and seeded successfully.");
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"An error occurred while applying migrations: {ex.Message}");
+            }
+            #endregion
+
+            app.MapControllers();
+        }
+    }
+}
---
+++ AdminDashboard/Program.cs
@@ -0,0 +1,32 @@
+using AdminDashboard.Extentions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllersWithViews();
+        builder.Services.ConfigureApplicationServices(builder.Configuration);
+
+        var app = builder.Build();
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
+}
