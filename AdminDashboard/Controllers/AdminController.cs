58f11b0affe7e20f1e48155f1f7be24c0fd1de0c
uosef <uosefahmed0022@gmail.com>
2025-03-10 22:17:09 +0200

    Update authentication flow and UI for Admin section
    - Changed default route to point to Admin Login.
    - Restructured and styled dropdown menu in _Layout.cshtml.
    - Added jQuery and Bootstrap scripts for enhanced functionality.
    - Updated _ViewImports.cshtml to include new authentication DTOs.
    - Introduced new styles for dropdown in styles.css.
    - Created AdminController for handling user authentication.
    - Added Login.cshtml view for user login interface.
    - Included logo.png for branding in the login view.
    - Created Login.css for styling the login page.

---
+++ AdminDashboard/Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ZadElealm.Apis.Dtos.Auth;
+using ZadElealm.Core.Models.Identity;
+
+namespace AdminDashboard.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AdminController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Invalid login attempt.");
+                return RedirectToAction(nameof(Login));
+            }
+            var result = await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
+            if (!result.Succeeded && !await _userManager.IsInRoleAsync(user, "Admin"))
+            {
+                ModelState.AddModelError("", "Invalid login attempt.");
+                return View(model);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Login));
+        }
+    }
+}
