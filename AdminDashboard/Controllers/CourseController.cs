cdfca1c607b23cb729686ea3c6f08bccf625312f
uosef <uosefahmed0022@gmail.com>
2025-03-11 22:31:37 +0200

    Add Course management functionality and views
    Introduces a new CourseController for handling course actions, including listing, editing, and updating courses. A CourseDto class is created for data transfer, and views for editing and listing courses are added. The navigation is updated to include a link to the Courses section, and CSS files are modified to style the new views.

---
+++ AdminDashboard/Controllers/CourseController.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ZadElealm.Apis.Dtos;
+using ZadElealm.Core.Models;
+using ZadElealm.Core.Repositories;
+using ZadElealm.Core.Service;
+
+namespace AdminDashboard.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly IImageService _imageService;
+
+        public CourseController(IUnitOfWork unitOfWork, IMapper mapper, IImageService imageService)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _imageService = imageService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var courses =await _unitOfWork.Repository<Course>().GetAllAsync();
+            var mappedCourses = _mapper.Map<IReadOnlyList<Course>, IReadOnlyList<CourseDto>>(courses);
+
+            return View(mappedCourses);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var course = await _unitOfWork.Repository<Course>().GetByIdAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            var model = new AdminDashboard.Dto.CourseDto
+            {
+                Name = course.Name,
+                Description = course.Description,
+                Author = course.Author,
+                CourseLanguage = course.CourseLanguage,
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(AdminDashboard.Dto.CourseDto model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var courseExsist = await _unitOfWork.Repository<Course>().GetByIdAsync(model.Id);
+            if (courseExsist == null)
+            {
+                return NotFound();
+            }
+
+            courseExsist.Name = model.Name;
+            courseExsist.Description = model.Description;
+            courseExsist.Author = model.Author;
+            courseExsist.CourseLanguage = model.CourseLanguage;
+
+
+            if (model.Image != null)
+            {
+                var uploadedImage = await _imageService.UploadImageAsync(model.Image);
+                courseExsist.ImageUrl = uploadedImage;
+            }
+
+            await _unitOfWork.Complete();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
