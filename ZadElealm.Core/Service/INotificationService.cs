8f69637c875ea09979ea0b304c4f487bcdd5e888
uosef <uosefahmed0022@gmail.com>
2025-03-11 23:48:06 +0200

    Add notification service for user alerts
    Introduces the `INotificationService` interface and its implementation in the `NotificationService` class to handle user notifications. A new `NotificationServiceDto` class is created to encapsulate notification details.
    
    The `ApplicationServiceExtensions.cs` registers the `INotificationService` as a scoped service. The `EnrollCourseCommandHandler` and `QuizService` classes are updated to send notifications upon course enrollment and quiz completion, respectively.
    
    The `NotificationType` enum is modified to include new notification types while removing duplicates. These changes enhance user engagement by improving the notification system.

---
+++ ZadElealm.Core/Service/INotificationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZadElealm.Core.Models.ServiceDto;
+
+namespace ZadElealm.Core.Service
+{
+    public interface INotificationService
+    {
+        Task SendNotificationAsync(NotificationServiceDto notificationServiceDto);
+    }
+}
---
+++ ZadElealm.Service/AppServices/NotificationService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZadElealm.Core.Enums;
+using ZadElealm.Core.Models;
+using ZadElealm.Core.Models.ServiceDto;
+using ZadElealm.Core.Repositories;
+using ZadElealm.Core.Service;
+
+namespace ZadElealm.Service.AppServices
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public NotificationService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task SendNotificationAsync(NotificationServiceDto notificationServiceDto)
+        {
+            var notification = new Notification
+            {
+                Title = notificationServiceDto.Title,
+                Description = notificationServiceDto.Description,
+                Type = notificationServiceDto.Type,
+                UserNotifications = new List<UserNotification>()
+            };
+            var userNotification = new UserNotification
+            {
+                UserId = notificationServiceDto.UserId,
+                Notification = notification,
+                IsRead = false
+            };
+            notification.UserNotifications.Add(userNotification);
+            await _unitOfWork.Repository<Notification>().AddAsync(notification);
+            await _unitOfWork.Complete();
+        }
+    }
+}
