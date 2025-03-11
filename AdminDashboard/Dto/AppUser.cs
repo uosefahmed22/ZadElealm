bbf6b829d44e2dd1fadb325c2e7a9faad039d2ab
uosef <uosefahmed0022@gmail.com>
2025-03-11 05:40:46 +0200

    Enhance role management and update project configuration
    Updated `AdminDashboard.csproj` to prevent errors on duplicate publish output files. Improved `RoleController.cs` with formatting changes and added checks to prevent deletion of critical roles. Modified `Program.cs` to allow developer exception page in both environments and removed unnecessary middleware. Updated `Index.cshtml` to disable buttons for "Admin" and "User" roles.
    
    Additionally, introduced new DTO classes in `AdminDto.cs`, `AppUser.cs`, `CategoryResponseDto.cs`, `CreateCategoryDto.cs`, and `LoginDTO.cs` to enhance data management and validation.

---
+++ AdminDashboard/Dto/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ZadElealm.Core.Models;
+
+namespace AdminDashboard.Dto
+{
+    public class AppUser : IdentityUser
+    {
+        public string DisplayName { get; set; }
+        public string? ImageUrl { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? DeletedAt { get; set; }
+        public ICollection<UserNotification> UserNotifications { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+        public ICollection<Favorite> Favorites { get; set; }
+        public ICollection<Rating> Ratings { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+        public ICollection<Report> Reports { get; set; }
+        public ICollection<Certificate> Certificates { get; set; }
+        public ICollection<Progress> Progresses { get; set; }
+    }
+}
--- AdminDashboard/Program.cs
+++ AdminDashboard/Program.cs
@@ -10,11 +10,15 @@ public class Program
         builder.Services.ConfigureApplicationServices(builder.Configuration);
 
         var app = builder.Build();
-        if (!app.Environment.IsDevelopment())
+        if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
         {
-            app.UseExceptionHandler("/Home/Error");
-            app.UseHsts();
+            app.UseDeveloperExceptionPage();
         }
+
+        app.UseCors(builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
 
         app.UseHttpsRedirection();
         app.UseStaticFiles();
