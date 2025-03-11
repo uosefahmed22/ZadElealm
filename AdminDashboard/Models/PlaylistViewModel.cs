7478bc6049ac7dbd6c3ca1bf341159222ed21f39
uosef <uosefahmed0022@gmail.com>
2025-03-11 22:58:34 +0200

    Add playlist creation feature and UI enhancements
    Updated CourseController to handle playlist creation with YouTube API integration. Added PlaylistViewModel for input validation. Introduced Create.cshtml for user interface and Createcourse.css for styling improvements.

---
+++ AdminDashboard/Models/PlaylistViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdminDashboard.Models
+{
+    public class PlaylistViewModel
+    {
+        [Required]
+        public string PlaylistUrl { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public string? CourseLanguage { get; set; }
+        public string? Author { get; set; }
+    }
+}
