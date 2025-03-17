d6d7d2a886d7a46572808639d43f468fe217cd84
uosef <uosefahmed0022@gmail.com>
2025-03-17 05:15:23 +0200

    Refactor Quiz functionality with MediatR integration
    Updated `QuizController` to use MediatR for commands and queries, simplifying data retrieval and quiz creation logic. Restructured `Create.cshtml` for improved layout and styling, and enhanced `Index.cshtml` for better presentation of quizzes. Updated CSS files for a modern look and maintainability. Added new command and query classes to adhere to CQRS principles, and modified `QuizWithCourseSpecification` to include course data.

---
+++ AdminDashboard/Handlers/QuizHandler/GetCoursesForQuizQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using AdminDashboard.Quires.QuizQuery;
+using MediatR;
+using ZadElealm.Core.Models;
+using ZadElealm.Core.Repositories;
+
+namespace AdminDashboard.Handlers.QuizHandler
+{
+    public class GetCoursesForQuizQueryHandler : IRequestHandler<GetCoursesForQuizQuery, IReadOnlyList<Course>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetCoursesForQuizQueryHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IReadOnlyList<Course>> Handle(GetCoursesForQuizQuery request, CancellationToken cancellationToken)
+        {
+            return await _unitOfWork.Repository<Course>().GetAllAsync();
+        }
+    }
+
+}
