369c46971d2bea0b110c438ebc5faa3a0788c621
uosef <uosefahmed0022@gmail.com>
2025-03-18 03:05:34 +0200

    Refactor CategoryController to use MediatR for queries
    - Updated `CategoryController` to utilize MediatR, replacing direct unit of work calls with the `GetAllCategoriesQuery`.
    - Removed `CategoryResponseDto` class to align with new query handling.
    - Enhanced UI in `Index.cshtml` with icons and improved layout for category items.
    - Updated `categories.css` with new styles for buttons, images, and responsive design.
    - Added `GetAllCategoriesQuery` and `GetAllCategoriesQueryHandler` to implement CQRS pattern.
    - Overall improvements in maintainability, readability, and user experience.

---
+++ AdminDashboard/Quires/GetAllCategoriesQuery.cs
@@ -0,0 +1,10 @@
+﻿using AdminDashboard.Dto;
+using MediatR;
+using ZadElealm.Apis.Dtos;
+
+namespace AdminDashboard.Quires
+{
+    public class GetAllCategoriesQuery : IRequest<IReadOnlyList<CategoryResponseDto>>
+    {
+    }
+}
