1b5a14d1357a7a02000e0f9db485a2070e9d7baa
uosef <uosefahmed0022@gmail.com>
2025-03-21 02:20:26 +0200

    Add profile update functionality and improve formatting
    - Introduced `UpdateProfile` method in `AccountController.cs` for user profile updates, including email validation.
    - Added regular expression validation for `DisplayName` in `RegisterDTO.cs` to enforce Arabic input.
    - Created `UpdateProfileCommand.cs` to encapsulate profile update data.
    - Added `UpdateProfileDto.cs` as a DTO for the update operation with validation attributes.
    - Implemented `UpdateProfileCommandHandler.cs` to handle the update logic and provide appropriate responses.

---
+++ ZadElealm.Apis/Commands/Auth/UpdateProfileCommand.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using System.ComponentModel.DataAnnotations;
+using ZadElealm.Apis.Errors;
+
+namespace ZadElealm.Apis.Commands.Auth
+{
+    public class UpdateProfileCommand : BaseCommand<ApiResponse>
+    {
+        public string? DisplayName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public UpdateProfileCommand( string? displayName, string? phoneNumber, string email)
+        {
+            Email = email;
+            DisplayName = displayName;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
---
+++ ZadElealm.Apis/Handlers/AuthHandler/UpdateProfileCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ZadElealm.Apis.Commands.Auth;
+using ZadElealm.Apis.Errors;
+using ZadElealm.Core.Models.Identity;
+using ZadElealm.Core.Service;
+
+namespace ZadElealm.Apis.Handlers.AuthHandler
+{
+    public class UpdateProfileCommandHandler : BaseCommandHandler<UpdateProfileCommand, ApiResponse>
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ISendEmailService _sendEmailService;
+
+        public UpdateProfileCommandHandler(UserManager<AppUser> userManager,
+            IOtpService otpService,
+            ISendEmailService sendEmailService)
+        {
+            _userManager = userManager;
+            _sendEmailService = sendEmailService;
+        }
+        public override async Task<ApiResponse> Handle(UpdateProfileCommand request, CancellationToken cancellationToken)
+        {
+            if (request == null)
+            {
+                return new ApiResponse(400, "لم يتم تحديث اي بيانات");
+            }
+            var user = await _userManager.FindByEmailAsync(request.Email);
+            if (user == null)
+            {
+                return new ApiResponse(404, "المستخدم غير موجود");
+            }
+            user.DisplayName = request.DisplayName;
+            user.PhoneNumber = request.PhoneNumber;
+            var result = await _userManager.UpdateAsync(user);
+            if (!result.Succeeded)
+            {
+                return new ApiResponse(400, "فشل تحديث البيانات");
+            }
+            return new ApiResponse(200, "تم تحديث البيانات بنجاح");
+        }
+    }
+}
