03653c496dbc3824ca6bf34148e045c7f8ae7a4b
uosef <uosefahmed0022@gmail.com>
2025-03-22 01:49:59 +0200

    Add user rating check feature in ReviewController
    Introduces a new HTTP GET endpoint `user-add-rateing-before/{courseId}` in the `ReviewController` to allow users to check if they can add a rating for a specific course.
    
    Adds `GetUserAddRateingBeforeQueryHandler` to handle the logic for checking existing ratings using a specification pattern, returning true or false based on the user's rating status.
    
    Also introduces `GetUserAddRateingBeforeQuery` class to encapsulate the parameters `CourseId` and `UserId` for the query.

---
+++ ZadElealm.Apis/Handlers/Review/GetUserAddRateingBeforeQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using ZadElealm.Apis.Errors;
+using ZadElealm.Apis.Quaries.Review;
+using ZadElealm.Core.Repositories;
+using ZadElealm.Core.Specifications.Rating;
+
+namespace ZadElealm.Apis.Handlers.Review
+{
+    public class GetUserAddRateingBeforeQueryHandler : BaseQueryHandler<GetUserAddRateingBeforeQuery, bool>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetUserAddRateingBeforeQueryHandler(
+            IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public override async Task<bool> Handle(GetUserAddRateingBeforeQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var spec = new RatingSpecification(request.UserId, request.CourseId);
+                var rating = await _unitOfWork.Repository<Core.Models.Rating>()
+                    .GetEntityWithSpecNoTrackingAsync(spec);
+
+                if (rating != null)
+                    return false;
+
+                return true;
+            }
+            catch (Exception)
+            {
+                throw new Exception("حدث خطأ أثناء التحقق من إضافة التقييم");
+            }
+        }
+    }
+}
---
+++ ZadElealm.Apis/Quaries/Review/GetUserAddRateingBeforeQuery.cs
@@ -0,0 +1,15 @@
+﻿using ZadElealm.Apis.Errors;
+
+namespace ZadElealm.Apis.Quaries.Review
+{
+    public class GetUserAddRateingBeforeQuery : BaseQuery<bool>
+    {
+        public int CourseId { get; set; }
+        public string UserId { get; set; }
+        public GetUserAddRateingBeforeQuery(int courseId, string userId)
+        {
+            CourseId = courseId;
+            UserId = userId;
+        }
+    }
+}
