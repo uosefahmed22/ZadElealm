c7be6babd8fcc4ead077bea307626ad150ed6665
uosef <uosefahmed0022@gmail.com>
2025-03-22 01:16:41 +0200

    Add CreatedAt property and update connection strings
    - Added `CreatedAt` property to `ReviewWithReviwerDataDto`.
    - Modified `DefaultConnection` in `appsettings.json` to include a local server connection string and retained the original public database connection string.

--- ZadElealm.Apis/Dtos/ReviewWithReviwerDataDto.cs
+++ ZadElealm.Apis/Dtos/ReviewWithReviwerDataDto.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Text { get; set; }
         public int CourseId { get; set; }
+        public DateTime CreatedAt { get; set; }
         public string AppUserId { get; set; }
         public string DisplayName { get; set; }
         public string ImageUrl { get; set; }
