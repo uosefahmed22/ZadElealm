655e8049bd8cfcbb4016c9027c97580658586c37
uosef <uosefahmed0022@gmail.com>
2025-03-26 05:49:56 +0200

    Refactor user management with MediatR and UI updates
    - Updated `UpdateUserRolesCommand` to use `UserRolesViewModel` for better encapsulation of user role data.
    - Removed `UserManager` and `RoleManager` from `UserController`, transitioning to MediatR for user operations.
    - Enhanced `Edit` method to accept `UserRolesViewModel` and updated response handling.
    - Introduced `DeleteUserCommand` and handler for user deletion.
    - Restructured `Edit.cshtml` for improved UI with Bootstrap cards and checkboxes for role management.
    - Improved layout and search functionality in `Index.cshtml`.
    - Updated `edituserroles.css` to use CSS variables for maintainability.
    - Modified `appsettings.json` to update the database connection string.

--- AdminDashboard/Handlers/UserHandler/UpdateUserRolesCommandHandler.cs
+++ AdminDashboard/Handlers/UserHandler/UpdateUserRolesCommandHandler.cs
@@ -17,10 +17,10 @@ namespace AdminDashboard.Handlers.UserHandler
 
         public async Task<ApiResponse> Handle(UpdateUserRolesCommand request, CancellationToken cancellationToken)
         {
-            var user = await _userManager.FindByIdAsync(request.UserId);
+            var user = await _userManager.FindByIdAsync(request.Model.UserId);
 
-            user.DisplayName = request.UserName;
-            user.IsDeleted = request.IsDeleted;
+            user.DisplayName = request.Model.UserName;
+            user.IsDeleted = request.Model.IsDeleted;
 
             var result = await _userManager.UpdateAsync(user);
             if (!result.Succeeded)
@@ -29,16 +29,19 @@ namespace AdminDashboard.Handlers.UserHandler
             }
 
             var userRoles = await _userManager.GetRolesAsync(user);
-            foreach (var role in request.Roles)
+            foreach (var role in request.Model.Roles)
             {
                 if (userRoles.Any(r => r == role.Name) && !role.IsSelected)
+                {
                     await _userManager.RemoveFromRoleAsync(user, role.Name);
-
+                }
                 if (!userRoles.Any(r => r == role.Name) && role.IsSelected)
+                {
                     await _userManager.AddToRoleAsync(user, role.Name);
+                }
             }
 
-            return new ApiResponse(200, "User updated successfully");
+            return new ApiResponse(200);
         }
     }
 }
